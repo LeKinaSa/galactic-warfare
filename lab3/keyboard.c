d1d90e72d83716300849bd69c6e16724b7399ea2
up201806332 <up201806332@812d746c-baee-4303-96b4-7e37b0e189f4>
2019-10-14 17:23:12 +0000

    Implemented kbd_subscribe_int and kbd_unsubscribe_int methods
    git-svn-id: https://svn.fe.up.pt/repos/lcom1920-t6g02@29 812d746c-baee-4303-96b4-7e37b0e189f4

--- lab3/keyboard.c
+++ lab3/keyboard.c
@@ -2,3 +2,30 @@
 
 #include "keyboard.h"
 #include "i8042.h"
+
+int hook_id = KBD_IRQ;
+
+int (kbd_subscribe_int)(uint8_t *bit_no) {
+  if (bit_no == NULL) {
+    printf("Error occurred: null pointer.\n");
+    return 1;
+  }
+
+  *bit_no = hook_id;
+
+  if (sys_irqsetpolicy(KBD_IRQ, IRQ_REENABLE | IRQ_EXCLUSIVE, &hook_id) != OK) {
+    printf("Error when calling sys_irqsetpolicy.\n");
+    return 1;
+  }
+
+  return 0;
+}
+
+int (kbd_unsubscribe_int)() {
+  if (sys_irqrmpolicy(&hook_id) != OK) {
+    printf("Error when calling sys_irqrmpolicy.\n");
+    return 1;
+  }
+
+  return 0;
+}
--- lab3/keyboard.h
+++ lab3/keyboard.h
@@ -1,4 +1,8 @@
 #ifndef __KEYBOARD_H
 #define __KEYBOARD_H
 
+#include <lcom/lcf.h>
+
+int (kbd_subscribe_int)(uint8_t *bit_no);
+
 #endif /* __KEYBOARD_H */
