d6f3d78f59b14ae18f86e0ee0e0fc76fcb603e1b
up201806528 <up201806528@812d746c-baee-4303-96b4-7e37b0e189f4>
2019-10-14 16:53:18 +0000

    Added some initial constants
    git-svn-id: https://svn.fe.up.pt/repos/lcom1920-t6g02@27 812d746c-baee-4303-96b4-7e37b0e189f4

--- lab3/i8042.h
+++ lab3/i8042.h
@@ -3,4 +3,14 @@
 
 #include <lcom/lcf.h>
 
+#define KBD_STATUS_REG           0x64                 /* Status Register Port */
+#define KBD_OUTPUT_BUF           0x60                 /* Output Buffer Port */
+#define KBD_INPUT_COMMANDS_BUF   KBD_STATUS_REG       /* Command Port */
+#define KBD_INPUT_ARGS_BUF       KBD_OUTPUT_BUF       /* Input arguments Port */
+
+//#define 
+
+#define KBD_IN_BUFF_FULL      BIT(1)    /* Bit set when input buffer is full */
+#define KBD_OUT_BUFF_FULL     BIT(0)    /* Bit set when output buffer is full */
+
 #endif /* _LCOM_I8042_H_ */
