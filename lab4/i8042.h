f5ea7a53b9a3036d5af6bb174537f8acd2c362f6
up201806528 <up201806528@812d746c-baee-4303-96b4-7e37b0e189f4>
2019-10-31 10:11:21 +0000

    Added files from other labs that will be needed in this lab
    git-svn-id: https://svn.fe.up.pt/repos/lcom1920-t6g02@65 812d746c-baee-4303-96b4-7e37b0e189f4

---
+++ lab4/i8042.h
@@ -0,0 +1,58 @@
+#ifndef _LCOM_I8042_H_
+#define _LCOM_I8042_H_
+
+#include <lcom/lcf.h>
+
+/* Constants for programming the i8042 KBC */
+
+#define KBD_IRQ   1     /* IRQ line for keyboard interrupts */
+
+/* PORTS */
+
+#define KBD_STATUS_REG           0x64                 /* Status register port */
+#define KBD_OUTPUT_BUF           0x60                 /* Output buffer port */
+#define KBD_INPUT_COMMANDS_BUF   KBD_STATUS_REG       /* KBC command input port */
+#define KBD_INPUT_ARGS_BUF       KBD_OUTPUT_BUF       /* Argument input port for KBC commands */
+
+
+/* STATUS REGISTER */
+
+#define KBD_PARITY_ERROR      BIT(7)    /* Bit set to 1 when there is a parity error */
+#define KBD_TIMEOUT           BIT(6)    /* Bit set to 1 when keyboard timeout */
+#define KBD_MOUSE_DATA        BIT(5)    /* Bit set to 1 if data comes from mouse */
+#define KBD_INHINIB_FLAG      BIT(4)    /* Bit set to 1 if keyboard is inhibit */
+#define KBD_A2                BIT(3)    /* Bit set to 1 if input is a command, 0 if input is data */
+#define KBD_SYS               BIT(2)    /* Bit set to 1 if system initialized, 1 if in reset */
+#define KBD_IN_BUF_FULL       BIT(1)    /* Bit set to 1 when input buffer is full */
+#define KBD_OUT_BUF_FULL      BIT(0)    /* Bit set to 1 when output buffer is full */
+
+/* KEYBOARD KBC COMMANDS */
+
+#define KBC_READ_COMMAND_BYTE  0x20      /* Read command byte */
+#define KBC_WRITE_COMMAND_BYTE 0x60      /* Write command byte */
+#define KBC_CHECK_SELF         0xAA      /* Check KBC (self test) */
+#define KBC_CHECK_INTERFACE    0xAB      /* Check KBC Interface */
+#define KBC_DISABLE_INTERFACE  0xAD      /* Disable KBD Interface */
+#define KBC_ENABLE_INTERFACE   0xAE      /* Enable KBD Interface */
+
+/* COMMAND BYTE */
+#define CMD_BYTE_DISABLE_MOUSE      BIT(5)    /* Bit set to 1 if mouse is disabled */
+#define CMD_BYTE_DISABLE_KBD        BIT(4)    /* Bit set to 1 if keyboard interface is disabled */
+#define CMD_BYTE_ENABLE_MOUSE_INT   BIT(1)    /* Bit set to 1 if mouse interrupts are enabled */
+#define CMD_BYTE_ENABLE_KBD_INT     BIT(0)    /* Bit set to 1 if keyboard interrupts are enabled */
+
+/* SCANCODES */
+
+#define KBD_BREAKCODE          BIT(7)    /* Bit set to 1 for breakcodes, 0 for makecodes */
+#define KBD_TWO_BYTE_CODE      0xE0      /* Most significant byte for a two byte scancode */
+#define KBD_ESC_BREAKCODE      0x81      /* ESC breakcode */
+
+/* POLLING */
+
+#define KBD_POLLING_INTERVAL   20000   /* Time in microseconds to wait between two polling cicles */
+
+
+#define KBD_TIMEOUT_MAX_ATTEMPTS  5
+#define KBC_COMMAND_WAIT_TIME     KBD_POLLING_INTERVAL * 50   /* Max wait time, in microseconds, when issuing a KBC command */
+
+#endif /* _LCOM_I8042_H_ */
---
+++ lab4/kbc.h
@@ -0,0 +1,12 @@
+#ifndef __KBC_H
+#define __KBC_H
+
+#include <lcom/lcf.h>
+
+/* Reusable functions related to the i8042 KBC */
+
+int kbc_read_status(uint8_t *status);     /* Reads status byte from 0x64 port */
+int kbc_write_command(uint8_t command);   /* Writes a KBC command to the 0x64 port */
+int kbc_write_arg(uint8_t arg);           /* Writes an argument for a KBC command to the 0x60 port */
+
+#endif /* __KBC_H */
