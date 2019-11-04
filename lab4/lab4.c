ba65242cd2988db9d0e2160227a60e07ca3b5726
up201806528 <up201806528@812d746c-baee-4303-96b4-7e37b0e189f4>
2019-11-04 19:01:15 +0000

    Initial mouse_test_gesture()
    git-svn-id: https://svn.fe.up.pt/repos/lcom1920-t6g02@87 812d746c-baee-4303-96b4-7e37b0e189f4

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -274,8 +274,86 @@ int (mouse_test_async)(uint8_t idle_time) {
 }
 
 
+enum states {
+  start_over,
+  going_up,
+  on_vertice,
+  going_down,
+  finished
+};
+
 int (mouse_test_gesture)(uint8_t x_len, uint8_t tolerance) {
-    // To be completed
-    printf("%s: under construction\n", __func__);
+    uint8_t bit_no = 0;
+
+  if (mouse_enable_data_report()) {
+    printf("Error when calling mouse_enable_data_report.\n");
     return 1;
+  }
+
+  if (mouse_subscribe_int(&bit_no)) {
+    printf("Error when calling mouse_subscribe_int.\n");
+    return 1;
+  }
+
+  int ipc_status;
+  message msg;
+
+  uint32_t num_packets = 0;
+  uint8_t packet_bytes[MOUSE_PCK_NUM_BYTES];
+  int packet_byte_counter = 0;
+  struct packet p;
+  
+  while (true) { // STOP CONDITION
+    if (driver_receive(ANY, &msg, &ipc_status)) { 
+      printf("Error when calling driver_receive.\n");
+      continue;
+    }
+    
+    if (is_ipc_notify(ipc_status)) {
+      switch(_ENDPOINT_P(msg.m_source)) {
+      case HARDWARE:
+        if (msg.m_notify.interrupts & BIT(bit_no)) {
+          mouse_ih();
+
+          if ((packet_byte_counter == MOUSE_INDEX_FIRST_BYTE) && ((packet_byte & MOUSE_FIRST_BYTE_CHECK) == 0)) {
+            continue;
+          }
+          
+          packet_bytes[packet_byte_counter] = packet_byte;
+
+          if (packet_byte_counter == MOUSE_INDEX_THIRD_BYTE) {
+            packet_byte_counter = 0;
+            ++num_packets;
+
+            mouse_parse_packet(packet_bytes, &p);
+            mouse_print_packet(&p);
+            // update_state_machine();
+          }
+          else {
+            ++packet_byte_counter;
+          }
+        }
+        break;
+      default:
+        break;
+      }
+    }
+  }
+
+  if (mouse_unsubscribe_int()) {
+    printf("Error when calling mouse_unsubscribe_int.\n");
+    return 1;
+  }
+
+  if (mouse_disable_data_report()) {
+    printf("Error when calling mouse_disable_data_report.\n");
+    return 1;
+  }
+
+  if (kbc_reset_cmd_byte()) {
+    printf("Error when calling kbc_reset_cmd_byte.\n");
+    return 1;
+  }
+
+  return 0;
 }
