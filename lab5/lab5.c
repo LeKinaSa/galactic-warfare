c459667ecd887590a1a7136e6c942477bee6221b
up201806528 <up201806528@812d746c-baee-4303-96b4-7e37b0e189f4>
2019-11-18 17:58:12 +0000

    First implementation of video_test_move()
    git-svn-id: https://svn.fe.up.pt/repos/lcom1920-t6g02@117 812d746c-baee-4303-96b4-7e37b0e189f4

--- lab5/lab5.c
+++ lab5/lab5.c
@@ -227,11 +227,86 @@ int(video_test_xpm)(xpm_map_t xpm, uint16_t x, uint16_t y) {
 
 int(video_test_move)(xpm_map_t xpm, uint16_t xi, uint16_t yi, uint16_t xf, uint16_t yf,
                      int16_t speed, uint8_t fr_rate) {
-  /* To be completed */
-  printf("%s(%8p, %u, %u, %u, %u, %d, %u): under construction\n",
-         __func__, xpm, xi, yi, xf, yf, speed, fr_rate);
+  uint16_t mode = MODE_GRAPHIC_INDEXED;
+  uint8_t kbd_bit_no = 0;
+  uint8_t timer0_bit_no = 0;
 
-  return 1;
+  if (timer_subscribe_int(&timer0_bit_no)) {
+    printf("Error when calling timer_subscribe_int.\n");
+    return 1;
+  }
+
+  if (kbd_subscribe_int(&kbd_bit_no)) {
+    printf("Error when calling kbd_subscribe_int.\n");
+    if (timer_unsubscribe_int()) {
+      printf("Error when calling timer_unsubscribe_int.\n");
+    }
+    return 1;
+  }
+
+  frame_buffer = vg_init(mode);
+
+  if (frame_buffer == MAP_FAILED) {
+    printf("Error occurred: couldn't map video memory.\n");
+    if (timer_unsubscribe_int()) {
+      printf("Error when calling timer_unsubscribe_int.\n");
+    }
+    if (kbd_unsubscribe_int()) {
+      printf("Error when calling kbd_unsubscribe_int.\n");
+    }
+    return 1;
+  }
+
+  int ipc_status;
+  message msg;
+
+  while (scancode != KBD_ESC_BREAKCODE) {
+    if (driver_receive(ANY, &msg, &ipc_status)) {
+      printf("Error when calling driver_receive.\n");
+      continue;
+    }
+    if (is_ipc_notify(ipc_status)) {
+      switch (_ENDPOINT_P(msg.m_source)) {
+      case HARDWARE:
+        if (msg.m_notify.interrupts & BIT(kbd_bit_no)) {
+          kbc_ih();
+        }
+        if (msg.m_notify.interrupts & BIT(timer0_bit_no)) {
+          //vg_draw_xpm(xpm, xi, yi, mode);
+        }
+        break;
+      default:
+        break;
+      }
+    }
+  }
+
+
+  if (vg_exit()) {
+    printf("Error when calling vg_exit.\n");
+    if (timer_unsubscribe_int()) {
+      printf("Error when calling timer_unsubscribe_int.\n");
+    }
+    if (kbd_unsubscribe_int()) {
+      printf("Error when calling kbd_unsubscribe_int.\n");
+    }
+    return 1;
+  }
+
+  if (kbd_unsubscribe_int()) {
+    printf("Error when calling kbd_unsubscribe_int.\n");
+    if (timer_unsubscribe_int()) {
+      printf("Error when calling timer_unsubscribe_int.\n");
+    }
+    return 1;
+  }
+
+  if (timer_unsubscribe_int()) {
+    printf("Error when calling timer_unsubscribe_int.\n");
+    return 1;
+  }
+  
+  return 0;
 }
 
 int(video_test_controller)() {
